304baab05babcd738f1912e91e312635642428ff
Frank Lemanschik <117739566+lemanschik@users.noreply.github.com>
2023-06-20 05:00:23 +0200

    Update index.tsx

--- interface/index.tsx
+++ interface/index.tsx
@@ -1,4 +1,4 @@
-import { Integrations, init } from '@sentry/browser';
+//import { Integrations, init } from '@sentry/browser';
 import '@fontsource/inter/variable.css';
 import { defaultContext } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
@@ -10,6 +10,8 @@ import { ErrorBoundary } from 'react-error-boundary';
 import { RouterProvider, RouterProviderProps } from 'react-router-dom';
 import { P2PContextProvider, useDebugState } from '@sd/client';
 import ErrorFallback from './ErrorFallback';
+
+// Maybe the only importent thing
 import { SpacedropUI } from './app/Spacedrop';
 
 export { ErrorPage } from './ErrorFallback';
@@ -21,29 +23,29 @@ dayjs.extend(advancedFormat);
 dayjs.extend(relativeTime);
 dayjs.extend(duration);
 
-init({
-	dsn: 'https://2fb2450aabb9401b92f379b111402dbc@o1261130.ingest.sentry.io/4504053670412288',
-	environment: import.meta.env.MODE,
-	defaultIntegrations: false,
-	integrations: [new Integrations.HttpContext(), new Integrations.Dedupe()]
-});
+// init({
+// 	dsn: 'https://2fb2450aabb9401b92f379b111402dbc@o1261130.ingest.sentry.io/4504053670412288',
+// 	environment: import.meta.env.MODE,
+// 	defaultIntegrations: false,
+// 	integrations: [new Integrations.HttpContext(), new Integrations.Dedupe()]
+// });
 
-const Devtools = () => {
-	const debugState = useDebugState();
+// const Devtools = () => {
+// 	const debugState = useDebugState();
 
-	// The `context={defaultContext}` part is required for this to work on Windows.
-	// Why, idk, don't question it
-	return debugState.reactQueryDevtools !== 'disabled' ? (
-		<ReactQueryDevtools
-			position="bottom-right"
-			context={defaultContext}
-			toggleButtonProps={{
-				tabIndex: -1,
-				className: debugState.reactQueryDevtools === 'invisible' ? 'opacity-0' : ''
-			}}
-		/>
-	) : null;
-};
+// 	// The `context={defaultContext}` part is required for this to work on Windows.
+// 	// Why, idk, don't question it
+// 	return debugState.reactQueryDevtools !== 'disabled' ? (
+// 		<ReactQueryDevtools
+// 			position="bottom-right"
+// 			context={defaultContext}
+// 			toggleButtonProps={{
+// 				tabIndex: -1,
+// 				className: debugState.reactQueryDevtools === 'invisible' ? 'opacity-0' : ''
+// 			}}
+// 		/>
+// 	) : null;
+// };
 
 export const SpacedriveInterface = (props: { router: RouterProviderProps['router'] }) => {
 	return (
